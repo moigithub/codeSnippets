e9a0bf97aac40f29a2b058777d2bc523908ae034
moigithub <mcarlosman@gmail.com>
2017-05-29 21:24:09 -0500

    initial page layout

--- client/components/main.js
+++ client/components/main.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class Main extends React.Component {
 	render(){
 		return (
 			<div>
-				main component here
+				
 			</div>
 		);
 	}
 }
-ReactDOM.render(<Main />, document.getElementById("app"));
+
--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
-console.log("rdyyyyy");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/main';
+
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+
+
+//asdfdsf
